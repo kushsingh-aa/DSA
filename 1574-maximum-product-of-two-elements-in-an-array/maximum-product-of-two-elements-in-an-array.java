83b54d935983c2346eae5598c973f73001acfeb6
kushsingh-aa <01kushsingh@gmail.com>
2026-07-27 07:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (73.61%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -10,7 +10,19 @@ class Solution {
         // }
         // return max;
 
-        Arrays.sort(nums);
-        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+        // Arrays.sort(nums);
+        // return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+
+        int mx1=0;
+        int mx2=0;
+        for(int num:nums){
+            if(num>mx1){
+                mx2=mx1;
+                mx1=num;
+            }else if(num>mx2){
+                mx2=num;
+            }
+        }
+        return (mx1-1)*(mx2-1);
     }
 }
