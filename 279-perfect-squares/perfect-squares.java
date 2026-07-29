162cf98eec5d3da594c510868be0e26a2667edf9
kushsingh-aa <01kushsingh@gmail.com>
2026-07-29 07:42:37 +0530

    Time: 1 ms (99.25%) | Memory: 42.5 MB (96.67%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,17 +1,30 @@
 class Solution {
-    public int square(int n){
-        return 0;
-    }
+    // public int square(int n){
+    //     return 0;
+    // }
 
-    public int numSquares(int n) {
-        int[] a=new int[n+1];
-        for(int i=1;i<=n;i++){
-            a[i]=i;
-            for(int j=1;j*j<=i;j++){
-                int sq=j*j;
-                a[i]=Math.min(a[i],a[i-sq]+1);
-            }
+    // public int numSquares(int n) {
+    //     int[] a=new int[n+1];
+    //     for(int i=1;i<=n;i++){
+    //         a[i]=i;
+    //         for(int j=1;j*j<=i;j++){
+    //             int sq=j*j;
+    //             a[i]=Math.min(a[i],a[i-sq]+1);
+    //         }
+    //     }
+    //     return a[n];
+    // }
+    public boolean square(int n){
+        int s=(int)Math.sqrt(n);
+        return s*s==n;
+    }
+    public int numSquares(int n){
+        if(square(n)) return 1;
+        while(n%4==0) n/=4;
+        if(n%8==7) return 4;
+        for(int i=1;i*i<n;i++){
+            if(square(n-(i*i))) return 2;
         }
-        return a[n];
+        return 3;
     }
 }
