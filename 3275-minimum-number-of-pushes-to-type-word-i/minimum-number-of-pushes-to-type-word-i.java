f4ac536a4ad635cb059b77cb615530995ab710b6
kushsingh-aa <01kushsingh@gmail.com>
2026-07-30 08:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (89.00%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -2,9 +2,12 @@ class Solution {
     public int minimumPushes(String word) {
         int n=word.length();
         int cnt=0;
-        for(int i=0;i<n;i++){
-            cnt+=(i/8)+1;
+        int box=n/8;
+        int d=n%8;
+        for(int i=1;i<=box;i++){
+            cnt+=i*8;
         }
+        cnt+=d*(box+1);
         return cnt;
     }
 }
