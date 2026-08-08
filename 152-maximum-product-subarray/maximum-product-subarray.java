d0f2b33a0d0ac94da4a951ef52ad8044033c4075
kushsingh-aa <01kushsingh@gmail.com>
2026-08-08 17:29:57 +0530

    Time: 1 ms (92.94%) | Memory: 47.1 MB (73.78%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,30 +1,30 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int mx=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int prod=1;
-            for(int j=i;j<nums.length;j++){
-                prod*=nums[j];
-                mx=Math.max(mx,prod);
-            }
-        }
-        return mx;
-
-        // int mx=nums[0];
-        // int mn=nums[0];
-        // int mxAns=nums[0];
-        // for(int i=1;i<nums.length;i++){
-        //     int c=nums[i];
-        //     if(c<0){
-        //         int temp=mx;
-        //         mx=mn;
-        //         mn=temp;
+        // int mx=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int prod=1;
+        //     for(int j=i;j<nums.length;j++){
+        //         prod*=nums[j];
+        //         mx=Math.max(mx,prod);
         //     }
-        //     mx=Math.max(c,mx*c);
-        //     mn=Math.min(c,mn*c);
-
-        //     mxAns=Math.max(mxAns,mx);
         // }
-        // return mxAns;
+        // return mx;
+
+        int mx=nums[0];
+        int mn=nums[0];
+        int mxAns=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int c=nums[i];
+            if(c<0){
+                int temp=mx;
+                mx=mn;
+                mn=temp;
+            }
+            mx=Math.max(c,mx*c);
+            mn=Math.min(c,mn*c);
+
+            mxAns=Math.max(mxAns,mx);
+        }
+        return mxAns;
     }
 }
