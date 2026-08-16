96ba5df53543f091df8b9f9992ac3821eef979ee
kushsingh-aa <01kushsingh@gmail.com>
2026-08-16 16:02:16 +0530

    Time: 7 ms (29.05%) | Memory: 114.6 MB (94.59%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean stoneGameIX(int[] stones) {
-        int[] a=new int[3];
-        Arrays.fill(a,0);
+       int cnt0=0,cnt1=0,cnt2=0;
         for(int stone:stones){
-            a[stone%3]++;
+            int rem=stone%3;
+            if(rem==0){
+                cnt0++;
+            }else if(rem==1){
+                cnt1++;
+            }else{
+                cnt2++;
+            }
         }
-        if(a[0]%2==0){
-            return Math.min(a[1],a[2])>0;
+        if(cnt0%2==0){
+            return cnt1>=1 && cnt2>=1;
         }
-        return Math.abs(a[1]-a[2])>2;
+        return Math.abs(cnt1-cnt2)>2;
     }
 }
