fe04023002f6d5fdf4fa1ac80478bb9152ea7f53
kushsingh-aa <01kushsingh@gmail.com>
2026-08-18 22:24:41 +0530

    Time: 1 ms (60.16%) | Memory: 45.3 MB (17.60%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int a=1;
+        if(nums.length==1){
+            return true;
+        }
+        for(int i=0;i<nums.length-1;i++){
+            if((nums[i]%2==0 && nums[i+1]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0)){
+                a=-1;
+            }
+        }
+        if(a==1) return true;
+        else return false;
+    }
+}
