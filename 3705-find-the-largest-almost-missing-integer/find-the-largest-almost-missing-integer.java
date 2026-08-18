5c7b08fd07b0e2d0e3a8800d35c82f5cf6eb1f3b
kushsingh-aa <01kushsingh@gmail.com>
2026-08-18 22:06:06 +0530

    Time: 4 ms (56.84%) | Memory: 46.5 MB (17.54%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,45 +1,15 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n=nums.length;
-        if(k==n){
-            int ans=-1;
-            for(int num:nums){
-                ans=Math.max(ans,num);
-            }
-            return ans;
+        int[] f=new int[51];
+        for(int i=0;i<=n-k;i++){
+           Set<Integer> set=new HashSet<>();
+            for(int j=i;j<i+k;j++) set.add(nums[j]);
+            for(int x:set) f[x]++;
         }
-
-        if(k==1){
-            Map<Integer,Integer> mp=new HashMap<>();
-            for(int num:nums){
-                mp.put(num,mp.getOrDefault(num,0)+1);
-            }
-            int ans=-1;
-            for(int num:mp.keySet()){
-                if(mp.get(num)==1){
-                    ans=Math.max(ans,num);
-                }
-            }
-            return ans;
+        for(int i=50;i>=0;i--){
+            if(f[i]==1) return i;
         }
-
-        int first=nums[0];
-        int last=nums[n-1];
-        int fcnt=0;
-        int lcnt=0;
-        for(int num:nums){
-            if(num==first) fcnt++;
-            if(num==last) lcnt++;
-        }
-
-        if(first==last){
-            return fcnt==1?first:-1;
-        }
-        
-        int ans=-1;
-        if(fcnt==1) ans=Math.max(ans,first);
-        if(lcnt==1) ans=Math.max(ans,last);
-
-        return ans;
+        return -1;
     }
 }
