14d64a9103d2e373c8d42cd7a8e99867d210329c
kushsingh-aa <01kushsingh@gmail.com>
2026-08-31 16:22:33 +0530

    Time: 1 ms (98.16%) | Memory: 43.2 MB (95.40%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int eSum=0;
+        int oSum=0;
+        for(int i=0;i<num.length();i++){
+            int d=num.charAt(i)-'0';
+            if(i%2==0){
+                eSum+=d;
+            }else{
+                oSum+=d;
+            }
+        }
+        return eSum==oSum;
+    }
+}
