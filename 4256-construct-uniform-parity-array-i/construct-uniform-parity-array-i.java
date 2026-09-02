e7675378048cdd76324c110c5985025469546824
kushsingh-aa <01kushsingh@gmail.com>
2026-09-02 11:10:07 +0530

    Time: 1 ms (31.89%) | Memory: 45.1 MB (73.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int n=nums1.length;
+        // int []nums2=new int[n];
+        // int j=-1;
+        // for(int i=0;i<n;i++){
+        //     if(nums1[i]%2!=0){
+        //         j=i;
+        //         break;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(j==-1){
+        //         nums2[i]=nums1[i];
+        //     }
+        //     else{
+        //         if(nums1[i]%2!=0){
+        //             nums2[i]=nums1[i];
+        //         }
+        //         else{
+        //             nums2[i]=nums1[i]-nums1[j];
+        //         }
+        //     }
+        // }
+        // return nums2!=nums1;
+        return true;
+    }
+}
