587988b801ff4a13f5cb82b5963328b598c9ed05
kushsingh-aa <01kushsingh@gmail.com>
2026-09-03 17:04:58 +0530

    Time: 2 ms (100.00%) | Memory: 121.6 MB (74.64%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int mn=Integer.MAX_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int num:nums1){
+            min=Math.min(min,num);
+        }
+        if(min%2==1){
+            return true;
+        }
         for(int num:nums1){
             if(num%2==1){
-                mn=Math.min(mn,num);
-            }
-        }
-
-        for(int num:nums1){
-            if(num%2==0 && mn!=Integer.MAX_VALUE && num<mn){
                 return false;
             }
         }
